d7cad0992edaf1aa99a82d150ec42b5998432017
Jeferson Ferreira <jeferson.oferreira@etec.sp.gov.br>
2025-09-02 22:12:15 -0300

    alteracoes

---
+++ 0209N/ClienteVip.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0209N
+{
+    class ClienteVip
+    {
+        private int pontos;
+
+        public void ConsultaPontos()
+        {
+
+        }
+
+
+    }
+}
