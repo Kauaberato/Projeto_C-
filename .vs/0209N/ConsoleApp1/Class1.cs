ae851951428aa23d7d5ad5a0d1573b47c2be438b
LuizOtavio264 <luiz.brasileiro@etec.sp.gov.br>
2025-09-02 22:20:51 -0300

    Add files via upload

---
+++ .vs/0209N/ConsoleApp1/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace heranca
+{
+    internal class Gerente : Supervisor {
+
+        public string setor;
+        public int Num_Ger;
+        public string grupo_resp;
+
+        public void ();
+
+
+    }
+    }
+}
---
+++ .vs/0209N/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace herenca
+{
+    internal class Program
+    {
+        namespace heranca
+    {
+        internal class Funcionario : Pessoa
+        {
+            public int registro;
+            public double salario;
+            public string funcao;
+            public string admissao;
+            public double valeRefei;
+            public double valeTrans;
+            public double valeAlimen;
+            public string assistenciaMedica;
+            public pessoa dependente;       
+
+
+            public void 
+        }
+    }
+}
